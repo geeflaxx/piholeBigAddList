173b3ba7c6d7bae1822633dc3751d5604f91d953
Patrick Destroy <patrick.seemann@stud.h-da.de>
2021-05-09 02:51:36 +0200

    upload project

---
+++ piholeBigAddList.py
@@ -0,0 +1,63 @@
+
+from selenium import webdriver
+import time
+
+
+print('################################################')
+print("use the script if you are confirm with links.txt")
+print('################################################')
+
+
+
+############ add your PiholeURL ############
+piholeURL = input("Your PiHole Adress:")
+piholeURL = "http://" + piholeURL
+
+############ Login ############
+print(f"Try Login to: {piholeURL}")
+password = input("password: ")
+driver = webdriver.Chrome()
+driver.get(f'{piholeURL}/admin/index.php?login')
+login = driver.find_element_by_id("loginpw")
+login.send_keys(password)
+login.submit()
+
+
+############ add list ############
+url = piholeURL + "/admin/groups-adlists.php"
+print(f"connected to {url}")
+driver.get(url)
+
+############ add all URL in your PiHole ############
+text = open('url.txt', 'r')
+
+urlField = driver.find_element_by_id('new_address')
+commentField = driver.find_element_by_id('new_comment')
+addBtn = driver.find_element_by_id('btnAdd')
+
+for line in text:
+    if line[0] == '#':
+        continue
+
+    urlField.send_keys(line)
+    commentField.send_keys('piholeBigAddlist-Script')
+    addBtn.click()
+    time.sleep(1)
+
+text.close()
+
+############ update ############
+
+url = piholeURL + "/admin/gravity.php"
+print(f"connected to :{url}")
+driver.get(url)
+
+updateField = driver.find_element_by_id("gravityBtn")
+updateField.click()
+
+
+
+
+
+
+
